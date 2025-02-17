291f22a0f51bf6030680d9892931598bf74e149c
Hazhir Naseri <94365660+hazhir00@users.noreply.github.com>
2025-02-17 15:33:09 +0330

    finish coding

--- script.js
+++ script.js
@@ -4,40 +4,40 @@ const close = document.getElementById("close");
 const hamburgerMenu = document.getElementById("hamburger-menu");
 
 bars.addEventListener("click", () => {
-   hamburgerMenu.style.transform = "translateX(0)";
+    hamburgerMenu.style.transform = "translateX(0)";
 });
 
 close.addEventListener("click", () => {
-   hamburgerMenu.style.transform = "translateX(-100%)";
+    hamburgerMenu.style.transform = "translateX(-100%)";
 });
 
 // ! makes the accordion menu work like an accordion
 document.querySelectorAll(".accordion-link").forEach((item) => {
-   item.addEventListener("click", () => {
-      const subMenu = item.querySelector("ul");
+    item.addEventListener("click", () => {
+        const subMenu = item.querySelector("ul");
 
-      // Close other submenus
-      document.querySelectorAll(".accordion-link ul").forEach((menu) => {
-         if (menu !== subMenu) {
-            menu.style.maxHeight = null;
-            menu.style.marginTop = "0px";
-         }
-      });
+        // Close other submenus
+        document.querySelectorAll(".accordion-link ul").forEach((menu) => {
+            if (menu !== subMenu) {
+                menu.style.maxHeight = null;
+                menu.style.marginTop = "0px";
+            }
+        });
 
-      // Toggle the clicked submenu
-      if (subMenu.style.maxHeight) {
-         subMenu.style.maxHeight = null;
-         subMenu.style.marginTop = "0px";
-      } else {
-         subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-         subMenu.style.marginTop = "0.5em";
-      }
-   });
+        // Toggle the clicked submenu
+        if (subMenu.style.maxHeight) {
+            subMenu.style.maxHeight = null;
+            subMenu.style.marginTop = "0px";
+        } else {
+            subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+            subMenu.style.marginTop = "0.5em";
+        }
+    });
 
-   // Prevent submenu from closing when clicking inside it
-   item.querySelectorAll("ul li").forEach((subMenuItem) => {
-      subMenuItem.addEventListener("click", (event) => {
-         event.stopPropagation();
-      });
-   });
+    // Prevent submenu from closing when clicking inside it
+    item.querySelectorAll("ul li").forEach((subMenuItem) => {
+        subMenuItem.addEventListener("click", (event) => {
+            event.stopPropagation();
+        });
+    });
 });
